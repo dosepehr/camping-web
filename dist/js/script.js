5df1b446ba287f756b219249b756846588a2d43a
dosepehr <dosepehr@gmail.com>
2023-03-26 23:48:41 +0430

    add overlay

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,15 @@
 const ham = document.querySelector('.ham');
 const menu = document.querySelector('.menu');
 const body = document.querySelector('body');
+const overlay = document.querySelector('.overlay');
 ham.onclick = () => {
+    ham.classList.toggle('open');
+    menu.classList.toggle('show');
+    overlay.classList.toggle('overlay-active');
+};
+
+overlay.onclick = () => {
+    overlay.classList.toggle('overlay-active');
     ham.classList.toggle('open');
     menu.classList.toggle('show');
 };
