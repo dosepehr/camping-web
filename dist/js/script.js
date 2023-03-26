0c9dddcdb3e75403dd66b906c8f2b4d697628cec
dosepehr <dosepehr@gmail.com>
2023-03-26 23:24:30 +0430

    add ham menu

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,7 @@
 const ham = document.querySelector('.ham');
+const menu = document.querySelector('.menu');
+const body = document.querySelector('body');
 ham.onclick = () => {
     ham.classList.toggle('open');
+    menu.classList.toggle('show');
 };
-
