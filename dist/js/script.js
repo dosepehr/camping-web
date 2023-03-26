ed499517640f515e9264739dfa84e9c5283ef038
dosepehr <dosepehr@gmail.com>
2023-03-27 00:24:19 +0430

    refactor codes

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,14 +2,17 @@ const ham = document.querySelector('.ham');
 const menu = document.querySelector('.menu');
 const body = document.querySelector('body');
 const overlay = document.querySelector('.overlay');
-ham.onclick = () => {
+
+const toggleMenu = () => {
     ham.classList.toggle('open');
     menu.classList.toggle('show');
     overlay.classList.toggle('overlay-active');
 };
 
+ham.onclick = () => {
+    toggleMenu();
+};
+
 overlay.onclick = () => {
-    overlay.classList.toggle('overlay-active');
-    ham.classList.toggle('open');
-    menu.classList.toggle('show');
+    toggleMenu();
 };
