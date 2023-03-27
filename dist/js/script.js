f0a881627420fae6dee9a1bf5faba42add2439a9
dosepehr <dosepehr@gmail.com>
2023-03-28 00:04:23 +0430

    add some animations

--- dist/js/script.js
+++ dist/js/script.js
@@ -16,3 +16,21 @@ ham.onclick = () => {
 overlay.onclick = () => {
     toggleMenu();
 };
+
+// ! animations
+const hiddenElements = document.querySelectorAll('.animate');
+console.log(hiddenElements);
+const observer = new IntersectionObserver(
+    (elements) => {
+        elements.forEach((element) => {
+            if (element.isIntersecting) {
+                element.target.classList.add('do-animate');
+            } else {
+                // element.target.classList.remove('show'); // by removing the else statement, the element will stay visible
+            }
+        });
+    },
+    { threshold: 0.5 }
+);
+
+hiddenElements.forEach((el) => observer.observe(el));
